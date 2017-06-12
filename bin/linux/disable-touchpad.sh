e28f01acbbc74f15d96b6b82bc6323639c9546b0
George Lewis <glewis@iqt.org>
2017-06-12 11:03:42 -0400

    more

--- bin/linux/disable-touchpad.sh
+++ bin/linux/disable-touchpad.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for i in `xinput |  awk '/TouchPad|Touchpad/' | sed -e 's/.*id=//' | sed -e 's/\s.*//'`; do
+for i in `xinput |  awk '/TouchPad|Touchpad|Touchscreen/' | sed -e 's/.*id=//' | sed -e 's/\s.*//'`; do
   xinput disable $i
 done
