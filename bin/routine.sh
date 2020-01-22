429fcf2ff2b27dd60b511888023dd068f60d4150
George Lewis <glewis@iqt.org>
2020-01-22 10:33:02 -0500

    update origin

--- bin/routine.sh
+++ bin/routine.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd ~/projects/q/dashboard && git pull origin master && git pull upstream master && git push origin master
+cd ~/projects/q/dashboard && git pull origin master && git pull origin master && git push origin master
 cd ~/projects/q/dashboard/routine-maintenance && ef routine-maintenance.json
-cd ~/projects/q/dashboard && git push origin master && git push upstream master
+cd ~/projects/q/dashboard && git push origin master && git push origin master
