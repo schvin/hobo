ef1cdca198cff27f02feca2ec5260964697fff2a
George Lewis <glewis@iqt.org>
2021-08-10 08:25:21 -0400

    main

--- bin/routine.sh
+++ bin/routine.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd ~/projects/q/dashboard && git pull origin master && git pull origin master && git push origin master
+cd ~/projects/q/dashboard && git pull origin main && git pull origin main && git push origin main
 cd ~/projects/q/dashboard/routine-maintenance && ef routine-maintenance.json
-cd ~/projects/q/dashboard && git push origin master && git push origin master
+cd ~/projects/q/dashboard && git push origin main && git push origin main
